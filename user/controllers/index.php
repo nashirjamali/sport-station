d3ef0d170e85bea12ceb5e18cda5211c5ab0db64
AyubRW <ayubrizki14@gmail.com>
2019-05-23 15:39:05 +0700

    user project

---
+++ user/controllers/index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sport Station</title>
+  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,700,300|Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/animate.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+</head>
+
+<body>
+  <!--header-->
+  <header class="main-header" id="header">
+    <div class="bg-color">
+      <!--nav-->
+      <nav class="nav navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="col-md-12">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mynavbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="fa fa-bars"></span>
+                        </button>
+              <a href="index.html" class="navbar-brand">Sport Station</a>
+            </div>
+            <div class="collapse navbar-collapse navbar-right" id="mynavbar">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#header">Home</a></li>
+                <li><a href="#feature">Fitur</a></li>
+                <li><a href="#portfolio">About Us</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#contact">Sign Up</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!--/ nav-->
+      <div class="container text-center">
+        <div class="wrapper wow fadeInUp delay-05s">
+          <h2 class="top-title">Sports & Daily Life</h2>
+          <h3 class="title">Sport Station</h3>
+          <h4 class="sub-title">Healthy Start From Ourselves</h4>
+          <button type="submit" class="btn btn-submit">Login</button>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!--/ header-->
+  <!---->
+  <section id="cta-1">
+    <div class="container">
+      <div class="row">
+        <div class="cta-info text-center">
+          <h3><span class="dec-tec">“</span>It’s your body. Tell it what to do.<span class="dec-tec">”</span> -Chris Powell</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!---->
+  <!---->
+  <section id="feature" class="section-padding">
+    <div class="container">
+      <div class="row">
+      <a href="">
+        <div class="col-md-3 wow fadeInLeft delay-05s">
+          <div class="section-title">
+            <h2 class="head-title">Fitur</h2>
+            <hr class="botm-line">
+            <p class="sec-para">Temukan kemudahan hidup sehat dengan berolahraga setiap hari, coba fitur di website kami. Jangan sampai kesibukanmu mengganggu kesehatanmu. </p>
+          </div>
+        </div>
+        </a>
+        <div class="col-md-9">
+        <a href="#" class="text-decoration-none">
+          <div class="col-md-6 wow fadeInRight delay-02s">
+            <div class="icon head-title">
+              <i class="fa fa-bullhorn"></i>
+            </div>
+            <div class="icon-text">
+              <h3 class="head-title">Event</h3>
+              <p class="txt-para">Temukan banyak informasi seputar event olahraga disekitar anda. Lets Join Us. </p>
+            </div>
+          </div>
+          </a>
+          <a href="#">
+          <div class="col-md-6 wow fadeInRight delay-02s">
+            <div class="icon head-title">
+              <i class="fa fa-map-marker"></i>
+            </div>
+            <div class="icon-text">
+              <h3 class="head-title">Lapangan</h3>
+              <p class="txt-para">Cari informasi lapangan sesuai kebutuhan olahraga anda, temukan kemudahan mencari tempat berolahraga. </p>
+            </div>
+          </div>
+          </a>
+          <a href="#">
+          <div class="col-md-6 wow fadeInRight delay-04s">
+            <div class="icon head-title">
+              <i class="fa fa-book"></i>
+            </div>
+            <div class="icon-text">
+              <h3 class="head-title">Edukasi</h3>
+              <p class="txt-para">Temukan cara hidup sehat dengan banyak cara dari segala usia. </p>
+            </div>
+          </div>
+          </a>
+          <a href="#">
+          <div class="col-md-6 wow fadeInRight delay-04s">
+            <div class="icon head-title">
+              <i class="fa fa-lightbulb-o"></i>
+            </div>
+            <div class="icon-text">
+              <h3 class="head-title">Program Diet</h3>
+              <p class="txt-para">Cara menurunkan berat badan tanpa bahaya kesehatan. </p>
+            </div>
+          </div>
+          </a>
+          <a href="#">
+          <div class="col-md-6 wow fadeInRight delay-06s">
+            <div class="icon head-title">
+              <i class="fa fa-truck"></i>
+            </div>
+            <div class="icon-text">
+              <h3 class="head-title">Persewaan</h3>
+              <p class="txt-para">Untuk memenuhi dan melengkapi kebutuhan olahraga anda setiap hari. </p>
+            </div>
+          </div>
+          </a>
+          <a href="#">
+          <div class="col-md-6 wow fadeInRight delay-06s">
+            <div class="icon head-title">
+              <i class="fa fa-shopping-cart"></i>
+            </div>
+            <div class="icon-text">
+              <h3 class="head-title">Penjualan</h3>
+              <p class="txt-para">Cari kebutuhan olahraga anda untuk menjaga kesehatan dan kebugaran diri anda. </p>
+            </div>
+          </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!---->
+  <section class="section-padding parallax bg-image-2 section wow fadeIn delay-08s" id="cta-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="cta-txt">
+            <h3>Bergabung Dengan Kami</h3>
+            <p>Dapatkan update seputar olahraga dan kesehatan setiap hari!</p>
+          </div>
+        </div>
+        <div class="col-md-4 text-center">
+          <a href="login.php" class="btn btn-submit">Lets Join Us</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!---->
+  <!---->
+  <section class="section-padding wow fadeInUp delay-02s" id="portfolio">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-12">
+          <div class="section-title">
+            <h2 class="head-title">About Us</h2>
+            <hr class="botm-line">
+            <p class="sec-para">Kami memberikan kemudahan untuk menjalani hidup sehat, dengan cara memberikan edukasi beserta 
+              persewaan dan penjualan alat olahraga dan event tentang olahraga. </p>
+          </div>
+        </div>
+        <div class="col-md-9 col-sm-12">
+          <div class="col-md-4 col-sm-6 padding-right-zero">
+            <div class="portfolio-box design">
+              <img src="img/port01.jpg" alt="" class="img-responsive">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 padding-right-zero">
+            <div class="portfolio-box design">
+              <img src="img/port02.jpg" alt="" class="img-responsive">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 padding-right-zero">
+            <div class="portfolio-box design">
+              <img src="img/port03.jpg" alt="" class="img-responsive">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 padding-right-zero">
+            <div class="portfolio-box design">
+              <img src="img/port04.jpg" alt="" class="img-responsive">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 padding-right-zero">
+            <div class="portfolio-box design">
+              <img src="img/port05.jpg" alt="" class="img-responsive">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 padding-right-zero">
+            <div class="portfolio-box design">
+              <img src="img/port06.jpg" alt="" class="img-responsive">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!---->
+  <!---->
+  <section class="section-padding wow fadeInUp delay-05s" id="contact">
+    <div class="container">
+      <div class="row white">
+        <div class="col-md-8 col-sm-12">
+          <div class="section-title">
+            <h2 class="head-title black">Contact Us</h2>
+            <hr class="botm-line">
+            <p class="sec-para black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..</p>
+          </div>
+        </div>
+        <div class="col-md-12 col-sm-12">
+          <div class="col-md-4 col-sm-6" style="padding-left:0px;">
+            <h3 class="cont-title">Email Us</h3>
+            <div id="sendmessage">Your message has been sent. Thank you!</div>
+            <div id="errormessage"></div>
+            <div class="contact-info">
+              <form action="" method="post" role="form" class="contactForm">
+                <div class="form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validation"></div>
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validation"></div>
+                </div>
+
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                  <div class="validation"></div>
+                </div>
+
+                <div class="form-group">
+                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                  <div class="validation"></div>
+                </div>
+                <button type="submit" class="btn btn-send">Send</button>
+              </form>
+            </div>
+
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <h3 class="cont-title">Visit Us</h3>
+            <div class="location-info">
+              <p class="white"><span aria-hidden="true" class="fa fa-map-marker"></span>Via Principe Amedeo 9, Torino, Italy</p>
+              <p class="white"><span aria-hidden="true" class="fa fa-phone"></span>Phone: 0039 333 12 68 347</p>
+              <p class="white"><span aria-hidden="true" class="fa fa-envelope"></span>Email: <a href="" class="link-dec">hi@yourdomain.com</a></p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="contact-icon-container hidden-md hidden-sm hidden-xs">
+              <span aria-hidden="true" class="fa fa-envelope-o"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!---->
+  <!---->
+  <footer class="" id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-7 footer-copyright">
+          © Bethany Theme - All rights reserved
+          <div class="credits">
+            <!--
+              All the links in the footer should remain intact.
+              You can delete the links only if you purchased the pro version.
+              Licensing information: https://bootstrapmade.com/license/
+              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Bethany
+            -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+        </div>
+        <div class="col-sm-5 footer-social">
+          <div class="pull-right hidden-xs hidden-sm">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-dribbble"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-google-plus"></i></a>
+            <a href="#"><i class="fa fa-pinterest"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!---->
+  <!--contact ends-->
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="../assets/js/jquery.easing.min.js"></script>
+  <script src="../assets/js/bootstrap.min.js"></script>
+  <script src="../assets/js/wow.js"></script>
+  <script src="../assets/js/custom.js"></script>
+  <script src="../assets/contactform/contactform.js"></script>
+
+</body>
+</html>
